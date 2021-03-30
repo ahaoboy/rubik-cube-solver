c58d1e0141a15e88260c1762e4530b60aaf531d9
ahaoboy <504595380@qq.com>
2021-03-31 00:47:34 +0800

    add test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,23 @@
-import { sum } from '../src';
-
-describe('blah', () => {
+import { Cube } from '../src/index';
+describe('solve', () => {
   it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+    const cubeState = [
+      'dbbburrfb', // front
+      'rudrruddl', // right
+      'flulfbddr', // up
+      'llffdrubf', // down
+      'rludlubrf', // left
+      'lubfbfudl', // back
+    ]
+      .join('')
+      .toUpperCase();
+
+    Cube.initSolver();
+    const cube = Cube.fromString(cubeState);
+    const path = cube.solve();
+    console.log(path);
+    expect(path).toEqual(
+      "U F B R L U2 B2 U L2 F' U' F2 U2 L2 D L2 F2 U' R2 U2 L2 U'"
+    );
   });
 });
